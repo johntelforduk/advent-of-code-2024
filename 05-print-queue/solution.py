0e459866f82b17643a5d3decd03d308d2675172a
johntelforduk <jtelford1@gmail.com>
2024-12-05 08:30:46 +0000

    Solution.

--- 05-print-queue/solution.py
+++ 05-print-queue/solution.py
@@ -13,11 +13,19 @@ assert middle([1,2,3]) == 2
 assert middle([2,3,4,5,6]) == 4
 
 
+def swap(l: list, a:int, b:int) -> list:
+    ai, bi = l.index(a), l.index(b)
+    l[bi], l[ai] = l[ai], l[bi]
+    return l
+
+
+assert swap([1, 2, 3, 4, 5], 3, 4) == [1, 2, 4, 3, 5]
+
+
 with open('input.txt', 'r') as file:
     instructions = file.read()
 
 rules_str, updates_str = instructions.split('\n\n')
-# ic(rules_str, updates_str)
 
 before_rules = {}
 after_rules = {}
@@ -37,10 +45,9 @@ for rule in rules_str.split('\n'):
 
 ic(before_rules, after_rules)
 
-part1 = 0
+part1, part2 = 0, 0
 for update_str in updates_str.split('\n'):
     update = [int(p) for p in update_str.split(',')]
-    # ic(update, middle(update))
 
     before = []
     correct = True
@@ -58,13 +65,25 @@ for update_str in updates_str.split('\n'):
                 if this in before_rules[a]:
                     correct = False
 
-        ic(before, this, update)
-
-
         before.append(this)
 
     if correct:
         part1 += middle(before)
-    ic(before, correct)
+    else:
+        # Part 2.
+        done = False
 
-ic(part1)
+        while not done:
+            done = True
+            i = 0
+            while i < len(before) - 1:
+                a, b = before[i], before[i + 1]
+                if b in before_rules:
+                    if a in before_rules[b]:
+                        swap(before, a, b)
+                        done = False
+
+                i += 1
+        part2 += middle(before)
+
+ic(part1, part2)
