824d5db3d7c10ceea28798273239da696a15db46
johntelforduk <jtelford1@gmail.com>
2024-12-10 19:30:47 +0000

    Part 2.

--- 10-hoof-it/part2.py
+++ 10-hoof-it/part2.py
@@ -2,6 +2,7 @@
 # https://adventofcode.com/2024/day/10
 
 from icecream import ic
+import pygame
 
 
 def trailhead(topo: dict, x: int, y: int) -> int:
@@ -15,25 +16,45 @@ def trailhead(topo: dict, x: int, y: int) -> int:
         if (xn, xy) in topo:
             if topo[(xn, xy)] == height + 1:
                 count += trailhead(topo, xn, xy)
-
     return count
 
 
-def trail_rating(topo: dict) -> int:
+def render(topo: dict, starts: dict, screen, scale: int):
+    background_colour = (255, 255, 255)           # Black.
+    screen.fill(background_colour)
+
+    for x, y in topo:
+        if (x, y) in starts:
+            tile_col = (6 * starts[(x, y)], 0, 0)
+        else:
+            tile_col = (0, (1 + topo[(x, y)]) * 20, 0)
+        pygame.draw.rect(screen, tile_col, pygame.Rect(x * scale, y * scale, scale, scale))
+
+    screenshot_name = 'screenshots/d10p2.png'
+    pygame.image.save(screen, screenshot_name)
+    pygame.display.flip()
+
+
+
+
+def trail_rating(topo: dict) -> (int, dict):
     total, progress = 0, 0
+    starts = {}
     for x, y in topo:
         if y > progress:
             progress = y
             ic(progress)
 
         if topo[(x, y)] == 0:
-            total += trailhead(topo, x, y)
-    return total
+            rating = trailhead(topo, x, y)
+            ic(rating)
+            starts[(x, y)] = rating
+            total += rating
+    return total, starts
 
 
 with open('input.txt', 'r') as file:
     topo_str = file.read()
-
 
 topo = {}
 y = 0
@@ -45,4 +66,11 @@ for line in topo_str.split('\n'):
         x += 1
     y += 1
 
-ic(trail_rating(topo))
+scale = 14
+screen_size = [scale * x, scale * y]  # [width, height]
+pygame.init()                                               # Initialize the game engine.
+screen = pygame.display.set_mode(screen_size)
+
+tr, starts = trail_rating(topo)
+ic(tr)
+render(topo, starts, screen, scale)
--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Solutions to AOC puzzles.
 ```commandline
 pip install icecream
 ```
+
+### Day 10: Hoof It
+
+Higher locations in lighter green. Trailheads are red. Lighter red means higher rating.
+
+![Trails](./10-hoof-it/screenshots/d10p2.png)
