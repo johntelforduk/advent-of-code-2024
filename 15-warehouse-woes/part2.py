3332f7bda45e1252d221501e5f7a52a02ffc3d89
johntelforduk <jtelford1@gmail.com>
2024-12-17 23:02:21 +0000

    First test passed.

--- 15-warehouse-woes/part2.py
+++ 15-warehouse-woes/part2.py
@@ -58,7 +58,7 @@ def pushable(wh:dict, tree:dict, dy: int) -> bool:
     return True
 
 
-def push(wh:dict, tree:dict, dy: int) -> bool:
+def push_vertical(wh:dict, tree:dict, dy: int) -> bool:
     """For parm set that contains a connected tree of boxes, and parm direction of travel (-1 or 1),
     move the boxes 1 space in that direction."""
 
@@ -70,28 +70,15 @@ def push(wh:dict, tree:dict, dy: int) -> bool:
     for x, y in tree:
         wh[(x, y + dy)] = tree[(x, y)]
 
-# def blocked(wh, rx, ry, dx, dy) -> bool:
-#     """Blocked if there are zero '.' between the robot and path to wall."""
-#     while wh[(rx, ry)] != '#':
-#         rx += dx
-#         ry += dy
-#         if wh[(rx, ry)] == '.':     # Found (at least) 1x gap, so robot is not blocked.
-#             return False
-#     return True                     # We reached a wall without finding a ',' so robot is blocked.
-#
-#
-# def push(wh, rx, ry, dx, dy):
-#     space_found = False
-#     nx, ny = rx, ry
-#     while not space_found:
-#         nx += dx
-#         ny += dy
-#         if wh[(nx, ny)] == '.':
-#             space_found = True
-#             wh[(nx, ny)] = 'O'
-#
-#     wh[(rx + dx, ry + dy)] = '.'
-#
+
+def blocked(wh, rx, ry, dx, dy) -> bool:
+    """Blocked if there are zero '.' between the robot and path to wall."""
+    while wh[(rx, ry)] != '#':
+        rx += dx
+        ry += dy
+        if wh[(rx, ry)] == '.':     # Found (at least) 1x gap, so robot is not blocked.
+            return False
+    return True                     # We reached a wall without finding a ',' so robot is blocked.
 #
 # def gps_calc(wh: dict) -> int:
 #     total = 0
@@ -99,6 +86,30 @@ def push(wh:dict, tree:dict, dy: int) -> bool:
 #         if wh[(x, y)] == 'O':
 #             total += x + 100 * y
 #     return total
+
+
+def push_horizontal(wh:dict, rx, ry, dx: int) -> bool:
+    # Look for space at end of this row.
+    if blocked(wh, rx, ry, dx, 0):
+        return False            # Robot not able to move.
+
+    boxes = {}
+    x = rx + dx
+    while wh[(x, ry)] not in '.#':
+        boxes[(x, ry)] = wh[(x, ry)]
+        x += dx
+
+    # ic(boxes)
+
+    # Everywhere the boxes used to be becomes space.
+    for x, y in boxes:
+        wh[(x, y)] = '.'
+
+    # Now put the boxes in their new positions.
+    for x, y in boxes:
+        wh[(x + dx, y)] = boxes[(x, y)]
+
+    return True             # Robot able to move.
 
 
 with open('test3.txt', 'r') as file:
@@ -127,12 +138,31 @@ for line in warehouse_str.split('\n'):
     my += 1
 
 
+render(wh, rx, ry, mx, my)
+
 deltas = {'>': (1, 0), 'v': (0, 1), '<': (-1, 0), '^': (0, -1)}
+for m in moves_str.replace('\n', ''):
+    dx, dy = deltas[m]
+
+    if wh[(rx + dx, ry + dy)] == '.':   # Move into space.
+        rx += dx
+        ry += dy
+    else:
+        if dx != 0:           # Horizontal move.
+            if push_horizontal(wh, rx, ry, dx):
+                rx += dx
+        else:                   # Vertical move.
+            tree = tree_of_boxes(wh, rx, ry + dy, dy)
+            # ic(tree)
+            if pushable(wh,tree, dy):
+                push_vertical(wh, tree, dy)
+                ry += dy
+
+    print(m)
+    render(wh, rx, ry, mx, my)
 
 
-# for m in moves_str.replace('\n', ''):
-#     dx, dy = deltas[m]
-#
+
 #     if not blocked(wh, rx, ry, dx, dy):
 #         push(wh, rx, ry, dx, dy)
 #         rx += dx
@@ -142,14 +172,32 @@ deltas = {'>': (1, 0), 'v': (0, 1), '<': (-1, 0), '^': (0, -1)}
 # render(wh, rx, ry, mx, my)
 # ic(gps_calc(wh))
 
-rx, ry = 7, 5
-dy = -1
-render(wh, rx, ry, mx, my)
-ic(rx, ry, dy)
 
-tree = tree_of_boxes(wh, rx, ry + dy, dy)
-ic(tree)
-ic(pushable(wh,tree, dy))
 
-push(wh, tree, dy)
-render(wh, rx, ry, mx, my)
+
+
+
+
+# rx, ry = 8, 4
+# dy = -1
+# render(wh, rx, ry, mx, my)
+# ic(rx, ry, dy)
+#
+# tree = tree_of_boxes(wh, rx, ry + dy, dy)
+# ic(tree)
+# ic(pushable(wh,tree, dy))
+#
+# push_vertical(wh, tree, dy)
+# render(wh, rx, ry, mx, my)
+#
+#
+# push_horizontal(wh,rx,ry, -1)
+# render(wh, rx, ry, mx, my)
+#
+# rx, ry = 5, 5
+# render(wh, rx, ry, mx, my)
+# tree = tree_of_boxes(wh, rx, ry + dy, dy)
+# ic(pushable(wh,tree, dy))
+# push_vertical(wh, tree, dy)
+# render(wh, rx, ry, mx, my)
+#
