38b841fcdbdaabb2b84c4c73f570660ba31c9b73
johntelforduk <jtelford1@gmail.com>
2024-12-26 17:11:05 +0000

    Bug in part 2.

---
+++ 22-monkey-market/part2.py
@@ -0,0 +1,96 @@
+# Advent of code day 22, Monkey Market.
+# https://adventofcode.com/2024/day/22
+
+from icecream import ic
+
+
+def mix(value: int, secret: int) -> int:
+    # To mix a value into the secret number, calculate the bitwise XOR of the given value and the secret number.
+    # Then, the secret number becomes the result of that operation.
+    return value ^ secret
+
+# If the secret number is 42 and you were to mix 15 into the secret number, the secret number would become 37.
+assert mix(15, 42) == 37
+
+
+def prune(secret: int) -> int:
+    # To prune the secret number, calculate the value of the secret number modulo 16777216.
+    # Then, the secret number becomes the result of that operation.
+    return secret % 16777216
+
+# If the secret number is 100000000 and you were to prune the secret number, the secret number would become 16113920.
+assert prune(100000000) == 16113920
+
+
+def iterate(secret: int) -> int:
+    # Calculate the result of multiplying the secret number by 64. Then, mix this result into the secret number.
+    # Finally, prune the secret number.
+    mult64 = secret * 64
+    secret = mix(mult64, secret)
+    secret = prune(secret)
+
+    # Calculate the result of dividing the secret number by 32. Round the result down to the nearest integer.
+    # Then, mix this result into the secret number. Finally, prune the secret number.
+    div32 = secret // 32
+    secret = mix(div32, secret)
+    secret = prune(secret)
+
+    # Calculate the result of multiplying the secret number by 2048. Then, mix this result into the secret number.
+    # Finally, prune the secret number.
+    mult2048 = secret * 2048
+    secret = mix(mult2048, secret)
+    secret = prune(secret)
+
+    return secret
+
+assert iterate(123) == 15887950
+assert iterate(15887950) == 16495136
+
+def unit_digit_delta(prev: int, latest: int) -> int:
+    return (latest % 10) - (prev % 10)
+
+assert unit_digit_delta(1234, 334457) == 3
+assert unit_digit_delta(65432, 291) == -1
+
+
+
+with open('test2.txt', 'r') as file:
+    buyers_str = file.read()
+
+distribution = {}
+
+for buyer_seed in [int(line) for line in buyers_str.split('\n')]:
+    output = buyer_seed
+    latest4 = []
+    for i in range(2000):
+        new_output = iterate(output)
+
+        udd = unit_digit_delta(prev=output, latest=new_output)
+        latest4.append(udd)
+
+        if len(latest4) > 4:
+            latest4.pop(0)
+
+        ic(buyer_seed, new_output % 10, latest4)
+
+        if len(latest4) == 4:
+            hash_latest4 = hash(tuple(latest4))
+            if hash_latest4 not in distribution:
+                distribution[hash_latest4] = (tuple(latest4), 1)
+            else:
+                _, count = distribution[hash_latest4]
+                distribution[hash_latest4] = (tuple(latest4), count + 1)
+
+        output = new_output
+
+# ic(len(distribution))
+
+best = None
+for hash_latest4 in distribution:
+    latest4_tuple, count = distribution[hash_latest4]
+    if best is None:
+        best = count
+    elif count > best:
+        best = count
+        ic(latest4_tuple, count)
+ic(best)
